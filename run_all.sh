495364ba4f4039ab46b36923da481a11b299582f
howard41436@gmail.com <howard41436@gmail.com>
2020-04-23 21:31:14 +0800

    add run_all

---
+++ run_all.sh
@@ -0,0 +1,102 @@
+echo starting FIFO_1
+./run.sh FIFO_1
+sleep 10
+echo FIFO_1 done
+
+echo starting FIFO_2
+./run.sh FIFO_2
+sleep 10
+echo FIFO_2 done
+
+echo starting FIFO_3
+./run.sh FIFO_3
+sleep 10
+echo FIFO_3 done
+
+echo starting FIFO_4
+./run.sh FIFO_4
+sleep 10
+echo FIFO_4 done
+
+echo starting FIFO_5
+./run.sh FIFO_5
+sleep 10
+echo FIFO_5 done
+
+
+echo starting RR_1
+./run.sh RR_1
+sleep 10
+echo RR_1 done
+
+echo starting RR_2
+./run.sh RR_2
+sleep 10
+echo RR_2 done
+
+echo starting RR_3
+./run.sh RR_3
+sleep 10
+echo RR_3 done
+
+echo starting RR_4
+./run.sh RR_4
+sleep 10
+echo RR_4 done
+
+echo starting RR_5
+./run.sh RR_5
+sleep 10
+echo RR_5 done
+
+
+echo starting SJF_1
+./run.sh SJF_1
+sleep 10
+echo SJF_1 done
+
+echo starting SJF_2
+./run.sh SJF_2
+sleep 10
+echo SJF_2 done
+
+echo starting SJF_3
+./run.sh SJF_3
+sleep 10
+echo SJF_3 done
+
+echo starting SJF_4
+./run.sh SJF_4
+sleep 10
+echo SJF_4 done
+
+echo starting SJF_5
+./run.sh SJF_5
+sleep 10
+echo SJF_5 done
+
+
+echo starting PSJF_1
+./run.sh PSJF_1
+sleep 10
+echo PSJF_1 done
+
+echo starting PSJF_2
+./run.sh PSJF_2
+sleep 10
+echo PSJF_2 done
+
+echo starting PSJF_3
+./run.sh PSJF_3
+sleep 10
+echo PSJF_3 done
+
+echo starting PSJF_4
+./run.sh PSJF_4
+sleep 10
+echo PSJF_4 done
+
+echo starting PSJF_5
+./run.sh PSJF_5
+sleep 10
+echo PSJF_5 done
