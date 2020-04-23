e04c8b12f5c89c8f29e6b0292b76e43c996ee602
howard41436@gmail.com <howard41436@gmail.com>
2020-04-23 20:32:45 +0800

    update run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 test=$1
 dmesg --clear
 ./scheduler < OS_PJ1_Test/$test.txt > output/${test}_stdout.txt
-dmesg | grep Project1 | cut -d ' ' -f 4- > output/${test}_dmesg.txt
+dmesg | grep Project1 | cut -d ']' -f 2- | cut -d ' ' -f 2- > output/${test}_dmesg.txt
